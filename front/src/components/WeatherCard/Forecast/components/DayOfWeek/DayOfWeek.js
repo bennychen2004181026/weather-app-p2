7a4601e346c02d605952d5ec324194a617bbc760
Ben <ben83026@ooutlook.com>
2024-02-02 17:20:39 +1100

    Create DayOfWeek

---
+++ front/src/components/WeatherCard/Forecast/components/DayOfWeek/DayOfWeek.js
@@ -0,0 +1,15 @@
+import WeatherIcon from '../../../WeatherIcon';
+import Name from '../../../Name';
+import Date from '../../../Date';
+import TemperatureRange from '../../../TemperatureRange';
+
+const DayOfWeek = () => (
+    <div>
+        <Name />
+        <Date />
+        <WeatherIcon />
+        <TemperatureRange />
+    </div>
+);
+
+export default DayOfWeek;
