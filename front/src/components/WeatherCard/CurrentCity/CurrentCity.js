4274ae4c023b3d8062867397b0d450ac9b372288
Ben <ben83026@ooutlook.com>
2024-02-02 17:36:16 +1100

    Create Meta, Image and Temperature in CurrentCity

--- front/src/components/WeatherCard/CurrentCity/CurrentCity.js
+++ front/src/components/WeatherCard/CurrentCity/CurrentCity.js
@@ -1,7 +1,23 @@
+import Meta from './components/Meta';
+import Temperature from './components/Temperature';
+import TemperatureRange from '../../TemperatureRange';
+import WeatherIcon from '../../WeatherIcon';
+import Date from '../../Date';
+import Name from '../../Name';
+import Image from './components/Image';
+import bg from './assets/Cloudy_day_background.png';
+import icon from '../../WeatherIcon/assets/Cloudy_day.png';
+
 const CurrentCity = () =>
 (
-    <div>
-        CurrentCity
+    <div className='flex flex-col items-center space-y-6 shadow-md'>
+        <Image imageUrl={ bg } alt='cloud' className='absolute top-0 right-0' />
+        <Date value='20 July, Sunday 12:00' className='text-white' />
+        <Name value='New York' className='text-white text-3xl font-bold' />
+        <Temperature value='20' className='text-slate-200 text-8xl font-bold ' />
+        <TemperatureRange value='18 ~ 23' className='text-white' />
+        <WeatherIcon imageUrl={ icon } alt='cloud' />
+        <Meta />
     </div>
 );
 
