c716b970ea73b5384ca172685e2ec1b94a425136
Ben <ben83026@ooutlook.com>
2024-02-02 17:48:59 +1100

    Update WeatherCard style

--- front/src/components/WeatherCard/WeatherCard.js
+++ front/src/components/WeatherCard/WeatherCard.js
@@ -1,15 +1,28 @@
 import CurrentCity from './CurrentCity';
 import Forecast from './Forecast';
-import OtherCites from './OtherCites';
+import OtherCities from './OtherCities';
 import SearchBar from './SearchBar';
 
 const WeatherCard = () =>
 (
-    <div className="w-4/5 h-4/5 mx-auto rounded-3xl bg-white overflow-hidden shadow-blue-800 grid grid-cols-6 grid-rows-6">
-        <CurrentCity />
-        <Forecast />
-        <OtherCites />
-        <SearchBar />
+    <div className="w-4/5 h-4/5 mx-auto rounded-3xl bg-white overflow-hidden shadow-blue-800 grid grid-cols-6 grid-rows-6" >
+        <div className="row-span-6 col-span-2 m-6 rounded-3xl  relative"
+            style={ {
+                backgroundImage:
+                    "linear-gradient(152deg, #899bf0 1%, #3f55e4 54%, #3f55e4 96%)",
+            } }
+        >
+            <CurrentCity />
+        </div>
+        <div className="row-span-3 col-span-4 border-8 m-6">
+            <Forecast />
+        </div>
+        <div className="row-span-1 col-span-4 border-8 m-6">
+            <SearchBar />
+        </div>
+        <div className="row-span-2 col-span-4 border-8 m-6">
+            <OtherCities />
+        </div>
     </div>
 );
 
