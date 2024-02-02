890959abf0e7fab02b22b6496a80394268a417e5
Ben <ben83026@ooutlook.com>
2024-02-02 17:12:35 +1100

    Create City component

---
+++ front/src/components/WeatherCard/OtherCites/City/City.js
@@ -0,0 +1,13 @@
+import WeatherIcon from '../../../WeatherIcon';
+import Name from '../../../Name';
+import TemperatureRange from '../../../TemperatureRange';
+
+const City = () => (
+    <div>
+        <WeatherIcon />
+        <Name />
+        <TemperatureRange />
+    </div>
+);
+
+export default City;
