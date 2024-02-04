3aaeb2d42d5e00d83f0e1b4dc46806cf89365705
Ben <ben83026@ooutlook.com>
2024-02-04 14:07:48 +1100

    Add SnackbarProvider

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,13 @@
 import WeatherCard from "./components/WeatherCard";
 import BackgroundImage from "./components/BackgroundImage";
 import bgImage from "./assets/bgImage.png";
+import { SnackbarProvider } from 'notistack';
 
 const App = () => (
-  <BackgroundImage imageUrl={ bgImage } alt="cloud" className="min-h-[800px] h-screen max-md:h-full max-md:py-6 max-xs:py-3 bg-cover bg-center flex items-center font-alimama" >
-    <WeatherCard />
-  </BackgroundImage>
+  <SnackbarProvider maxSnack={ 3 }>
+    <BackgroundImage imageUrl={ bgImage } alt="cloud" className="min-h-[800px] h-screen max-md:h-full max-md:py-6 max-xs:py-3 bg-cover bg-center flex items-center font-alimama" >
+      <WeatherCard />
+    </BackgroundImage>
+  </SnackbarProvider>
 );
 export default App;
