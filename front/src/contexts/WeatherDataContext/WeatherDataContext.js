7ae93345f5a641526a11e58fcb65039c3e1de02b
Ben <ben83026@ooutlook.com>
2024-02-04 17:24:34 +1100

    Create WeatherDataContext and provider

---
+++ front/src/contexts/WeatherDataContext/WeatherDataContext.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import useFetchWeatherData from '../../hooks/useFetchWeatherData';
+
+const WeatherDataContext = React.createContext( null );
+
+const WeatherDataProvider = ( { children } ) =>
+{
+    const { weatherData: SydneyData, error: SydneyError, isLoading: SydneyLoading } = useFetchWeatherData( 'Sydney', 'next4days' );
+    const { weatherData: ShanghaiData, error: ShanghaiError, isLoading: ShanghaiLoading } = useFetchWeatherData( 'Shanghai' );
+    const { weatherData: NewYorkData, error: NewYorkError, isLoading: NewYorkLoading } = useFetchWeatherData( 'NewYork' );
+    const { weatherData: LondonData, error: LondonError, isLoading: LondonLoading } = useFetchWeatherData( 'London' );
+
+    const totalData = {
+        data: [ SydneyData, ShanghaiData, NewYorkData, LondonData ],
+        error: SydneyError || ShanghaiError || NewYorkError || LondonError,
+        isLoading: SydneyLoading || ShanghaiLoading || NewYorkLoading || LondonLoading
+    };
+
+    return (
+        <WeatherDataContext.Provider value={ totalData }>{ children }</WeatherDataContext.Provider>
+    );
+};
