5942858e17c34640da63d946e6d177664aa57cf3
Ben <ben83026@ooutlook.com>
2024-02-06 17:04:17 +1100

    Fixed CloseButton className

--- front/src/components/CloseButton/CloseButton.js
+++ front/src/components/CloseButton/CloseButton.js
@@ -7,7 +7,7 @@ const CloseButton = ( { snackbarKey, closeSnackbar } ) => (
         onClick={ () => closeSnackbar( snackbarKey ) }
     >
         <span className="sr-only">Close menu</span>
-        <svg className="h-8 w-8 text-red-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
+        <svg className="h-8 w-8 text-red-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
     </button>
 );
 export default CloseButton;
